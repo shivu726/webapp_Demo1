5f41225fadb420abf91482a6eb2d380dfb900e57
shivu726 <69666040+shivu726@users.noreply.github.com>
2024-09-16 10:19:42 +0530

    Delete webapp_demo1/Models directory

--- webapp_demo1/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace webapp_demo1.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
